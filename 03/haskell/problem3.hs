9e6ac94d15fa4988d0929706ba05456ba2337af2
Mikail Khan <mikail.khan45@gmail.com>
2021-04-28 23:23:28 -0400

    improves haskell wheelfactor

--- 03/haskell/problem3.hs
+++ 03/haskell/problem3.hs
@@ -2,7 +2,7 @@ import Data.Maybe
 import Data.List (find)
 
 wheelFactor :: Int -> Int
-wheelFactor n = fromJust $ find ((==0) . rem n) testLS
+wheelFactor n = fromMaybe n $ find ((==0) . rem n) (takeWhile ((< n) . floor . sqrt . fromIntegral) testLS)
     where testLS = 2 : 3 : 5 : 7 : 11 : zipWith (+) (cycle incr) (drop 4 testLS)
           incr = [4, 2, 4, 2, 4, 6, 2, 6]
 
